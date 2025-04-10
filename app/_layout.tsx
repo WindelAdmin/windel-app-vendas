2d13ebea75ba8a6f5d2fcf69ed94438ebef23f9d
windel <francdrigue@gmail.com>
2025-04-10 17:57:08 -0300

    init project

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,20 @@
 import '../global.css';
 
 import { Stack } from 'expo-router';
+import { StatusBar} from "react-native"
 
 export default function Layout() {
-  return <Stack />;
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#0965E5"  />
+      <Stack
+        screenOptions={{
+         headerShown: false
+        }} 
+      >
+      <Stack.Screen name='index'/>
+      <Stack.Screen name='home'/>
+    </Stack>
+    </>
+  )
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,45 @@
-import { Stack, Link } from 'expo-router';
+import { 
+  View, 
+  Text,
+  KeyboardAvoidingView,
+  Platform,
+  TextInput
+} from "react-native";
+import {Ionicons} from "@expo/vector-icons"
 
-import { Button } from '~/components/Button';
-import { Container } from '~/components/Container';
-import { ScreenContent } from '~/components/ScreenContent';
-
-export default function Home() {
+export default function Login() {
   return (
-    <>
-      <Stack.Screen options={{ title: 'Home' }} />
-      <Container>
-        <ScreenContent path="app/index.tsx" title="Home" />
-        <Link href={{ pathname: '/details', params: { name: 'Dan' } }} asChild>
-          <Button title="Show Details" />
-        </Link>
-      </Container>
-    </>
+     <KeyboardAvoidingView 
+     className={styles.containerLogin}
+     behavior={Platform.OS === "ios" ? "padding" : "height"}
+     >
+        <View className={styles.containerHeader}>
+          <Text className={styles.title}>Bem Vindo(a)</Text>
+          <Text className={styles.subTitle}>Ao Windel Mobile</Text>
+        </View>
+        <View className={styles.containerForm}>
+          <View className={styles.containerTitlesForm}>
+            <Text className={styles.textTitleOneForm}>Venda com mais</Text>
+            <Text className={styles.textTitleTwoForm}>Praticidade!</Text>
+          </View>
+          <View>
+             <Ionicons name="person-circle" size={25} color={"#B6C5DA"}/>
+             <TextInput
+             placeholder="Usuário"
+             />
+          </View>
+        </View>
+     </KeyboardAvoidingView>
   );
+}
+
+const styles = {
+  containerLogin: `flex-1 bg-[#0965E5]`,
+  containerHeader: `mt-[50px] ml-[25px] mb-[40px]`,
+  containerForm: `flex-1 bg-[#F4F4F4] rounded rounded-s-3xl `,
+  title: `text-[27px] font-bold color-[#fff]`,
+  subTitle: `text-[25px] color-[#fff]`,
+  textTitleOneForm: `color-[#1448A8] text-[18px] font-bold`,
+  textTitleTwoForm: `color-[#7AA0D6] text-[25px]`,
+  containerTitlesForm: `mt-[35px] ml-[25px]`,
 }
