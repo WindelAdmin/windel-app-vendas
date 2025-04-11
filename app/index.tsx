e634b4ae4f24c15fdfc7625a32f152816cf9a849
Francisco <francdrigue@gmail.com>
2025-04-10 22:09:48 -0300

    adjustment screen login

--- app/index.tsx
+++ app/index.tsx
@@ -3,14 +3,18 @@ import {
   Text,
   KeyboardAvoidingView,
   Platform,
-  TextInput
+  TextInput,
+  TouchableOpacity
 } from "react-native";
-import {Ionicons} from "@expo/vector-icons"
+import {Ionicons} from "@expo/vector-icons";
+import { LinearGradient } from 'expo-linear-gradient';
+
 
 export default function Login() {
   return (
+    <LinearGradient className="flex-1"  colors={['#0965E5', '#0014EE', '#2773DC']} start={{ x: 0.4, y: 0.0 }}>
      <KeyboardAvoidingView 
-     className={styles.containerLogin}
+     className="flex-1"
      behavior={Platform.OS === "ios" ? "padding" : "height"}
      >
         <View className={styles.containerHeader}>
@@ -22,24 +26,42 @@ export default function Login() {
             <Text className={styles.textTitleOneForm}>Venda com mais</Text>
             <Text className={styles.textTitleTwoForm}>Praticidade!</Text>
           </View>
-          <View>
+          <View className={styles.input}>
              <Ionicons name="person-circle" size={25} color={"#B6C5DA"}/>
              <TextInput
-             placeholder="Usuário"
+               className="flex-1"
+               placeholder="Usuário"
              />
           </View>
+          <View className={styles.input}>
+              <Ionicons name="lock-closed" size={25} color={"#B6C5DA"}/>
+              <TextInput
+                className="flex-1"
+                placeholder="Senha"
+              />
+              <Ionicons name="eye" size={25} color={"#B6C5DA"}/>
+          </View>
+          <Text className={styles.forgotPassword}>Esqueceu a senha?</Text>
+
+           <TouchableOpacity className={styles.button}>
+             <Text className={styles.textButton}>Entrar</Text>
+           </TouchableOpacity>
         </View>
      </KeyboardAvoidingView>
+    </LinearGradient>
   );
 }
 
 const styles = {
-  containerLogin: `flex-1 bg-[#0965E5]`,
   containerHeader: `mt-[50px] ml-[25px] mb-[40px]`,
-  containerForm: `flex-1 bg-[#F4F4F4] rounded rounded-s-3xl `,
+  containerForm: `flex-1 bg-[#F4F4F4] rounded rounded-s-3xl px-[25px]`,
   title: `text-[27px] font-bold color-[#fff]`,
   subTitle: `text-[25px] color-[#fff]`,
   textTitleOneForm: `color-[#1448A8] text-[18px] font-bold`,
-  textTitleTwoForm: `color-[#7AA0D6] text-[25px]`,
-  containerTitlesForm: `mt-[35px] ml-[25px]`,
+  textTitleTwoForm: `color-[#7AA0D6] text-[25px] mt-[-6px]`,
+  containerTitlesForm: `mt-[35px]`,
+  input: `w-full flex-row bg-[#FFFFFF] rounded-md py-1 px-2 items-center gap-1 border border-[#C9C9C9] mt-[20px]`,
+  forgotPassword: `self-end color-[#7AA0D6] mt-[10px]`,
+  button: `bg-[#2773DC] rounded py-[8px] mt-[20px]`,
+  textButton: `text-[20px] color-[#fff] text-center`
 }
